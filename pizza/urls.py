4726a4df5d070603dc5589770e391b1c3010e9eb
jod35 <jodestrevin@gmail.com>
2021-10-05 09:08:24 +0300

    change static root

--- pizza/urls.py
+++ pizza/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/',include('authentication.urls')),
     path('',include('orders.urls')),
-    path('swagger<format>.json|.yaml', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger<format>.json|.yaml/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('auth/', include('djoser.urls.jwt')),
