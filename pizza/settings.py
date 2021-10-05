c8f8b7db1cb2df137cd956b18b98ab5982c276ad
jod35 <jodestrevin@gmail.com>
2021-10-05 08:52:01 +0300

    whitenoise settings

--- pizza/settings.py
+++ pizza/settings.py
@@ -153,7 +153,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT=BASE_DIR / 'static'
+STATIC_ROOT=BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
